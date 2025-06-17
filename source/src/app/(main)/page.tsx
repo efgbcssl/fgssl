69472bdef24e6c2b493fa755fe77e58fbc00cfaf
ObsidianBeing <obsidian.being.tech@gmail.com>
2025-06-17 17:36:52 +0300

    page.tsx fix

--- source/src/app/(main)/page.tsx
+++ source/src/app/(main)/page.tsx
@@ -8,13 +8,19 @@ import FAQSection from '@/components/home/FAQSection'
 import LiveStream from '../../components/home/LiveStream';
 
 async function getEvents() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/events`, {
-        next: { revalidate: 60 }, // Revalidate every 60 seconds
-    })
+    const baseUrl =
+        process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000'
+            : process.env.NEXT_PUBLIC_SITE_URL;
 
-    if (!res.ok) throw new Error('Failed to fetch events')
-    return res.json()
+    const res = await fetch(`${baseUrl}/api/events`, {
+        next: { revalidate: 60 },
+    });
+
+    if (!res.ok) throw new Error('Failed to fetch events');
+    return res.json();
 }
+
 
 export default async function Home() {
     const events = await getEvents();
